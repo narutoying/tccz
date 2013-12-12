2c5c433e8266e4deff2136205bf9eaacdbb10c4d
narutoying <narutoying09@gmail.com>
2013-12-12 21:13:23 +0800

    quartz接入

---
+++ app/core/service/src/main/java/com/tccz/tccz/core/service/manage/job/DataExportJob.java
@@ -0,0 +1,24 @@
+/**
+ * Taicang mscz Inc.
+ * Copyright (c) 2010-2013 All Rights Reserved.
+ */
+package com.tccz.tccz.core.service.manage.job;
+
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+
+/**
+ * 
+ * @author narutoying09@gmail.com
+ * @version $Id: DataExportJob.java, v 0.1 2013-12-12 下午5:11:03
+ *          narutoying09@gmail.com Exp $
+ */
+public class DataExportJob {
+	private static final Logger logger = LoggerFactory
+			.getLogger(DataExportJob.class);
+
+	public void exportExcel() {
+		logger.info("Export...");
+		System.out.println("hello");
+	}
+}
